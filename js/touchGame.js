15b10252f3d09855759507a158c677d847c43e23
Woo Jeongwook <dnwjddnr115@naver.com>
2024-07-17 16:02:42 +0900

    터치게임 완성 / setInterval로 오류 수정

--- js/touchGame.js
+++ js/touchGame.js
@@ -2,6 +2,12 @@ let screen = document.getElementById("screen");
 let note = document.getElementById("note");
 let start = document.getElementById("start");
 
+let timeP = document.getElementById("time");
+let scoreP = document.getElementById("score");
+
+let noteInv;
+let timeInv;
+
 let score = 0;
 let timer = 30;
 let x;
@@ -17,20 +23,36 @@ function setStart() {
 }
 
 function startGame() {
-  while (timer != 0) {
-    randomNote();
-    setTimeout(runTime, 1000);
-  }
+  noteInv = setInterval(randomNote, 1000);
+  timeInv = setInterval(runTime, 1000);
+
+  note.onclick = function () {
+    score++;
+    scoreP.innerText = score;
+    note.style.display = "none";
+  };
+}
+
+function stopGame() {
+  clearInterval(timeInv);
+  clearInterval(noteInv);
+  alert("게임이 종료되었습니다.\n당신의 점수는 " + score + "점입니다.");
+  location.href = "touchGame.html";
 }
 
 function runTime() {
+  if (timer == 0) {
+    stopGame();
+  }
   timer--;
+  timeP.innerText = timer;
 }
 
 function randomNote() {
-  x = Math.floor(Math.random() * 1600 + 60);
-  y = Math.floor(Math.random() * 800 + 60);
+  x = Math.floor(Math.random() * 1200 + 80);
+  y = Math.floor(Math.random() * 600 + 80);
   note.style.top = y + "px";
   note.style.left = x + "px";
   note.style.display = "block";
+  console.log(score);
 }
